3aa6e97f10de69a3b2e8b0766cf07a1b93d0e79a
James <james.a.forbes@gmail.com>
2015-01-31 22:14:42 +1100

    Leaflet js post

--- posts.js
+++ posts.js
@@ -20,14 +20,9 @@ posts = [
 		"created": "2014-11-22T08:56:20.959Z"
 	},
 	{
-		"name": "Pulling Date Stamps from Commits",
-		"path": "posts/Pulling Date Stamps from Commits.md",
-		"created": "2014-08-03T11:31:29Z"
-	},
-	{
-		"name": "No Tumblr But All is Well",
-		"path": "posts/No Tumblr But All is Well.md",
-		"created": "2014-08-03T10:25:53Z"
+		"name": "A better LeafletJS",
+		"path": "posts/noClasses.md",
+		"created":"2014-10-29T11:12:06.740Z"
 	},
 	{
 		"name": "Hosting via gh-pages",
@@ -38,6 +33,16 @@ posts = [
 		"name": "Google Glass",
 		"path": "posts/Google Glass.md",
 		"created": "2014-08-03T23:52:20Z"
+	},
+	{
+		"name": "Pulling Date Stamps from Commits",
+		"path": "posts/Pulling Date Stamps from Commits.md",
+		"created": "2014-08-03T11:31:29Z"
+	},
+	{
+		"name": "No Tumblr But All is Well",
+		"path": "posts/No Tumblr But All is Well.md",
+		"created": "2014-08-03T10:25:53Z"
 	},
 	{
 		"name": "Example Post",
