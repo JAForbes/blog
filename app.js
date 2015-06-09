430106f8e6b7113d97fe871f5cc95f50d7d94ecb
James Forbes <james.a.forbes@gmail.com>
2015-06-09 12:04:16 +1000

    Update app.js

--- app.js
+++ app.js
@@ -43,8 +43,8 @@ function Bio(){
 	return '<div class="bio">'+
 		'<img src="img/bio.jpeg"/>'+
 		'<p>' +"Hi!  I'm James! Programmer and a musician."+'</p>' +
-		'<a href="https://twitter.com/james_a_forbes"> (Twitter) </a>' + 
-		'<a href="https://github.com/JAForbes"> (GitHub) </a>' + 
+		'<p><a href="https://twitter.com/james_a_forbes"> (Twitter) </a></p>' + 
+		'<p><a href="https://github.com/JAForbes"> (GitHub) </a></p>' + 
 	'</div>'
 }
 
