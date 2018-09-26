cd0841d52ad19c7b4d927b7ea0dc7c5d96b350b4
James <james.a.forbes@gmail.com>
2018-09-27 00:04:21 +1000

    Remove unused title updater

--- app.js
+++ app.js
@@ -9,25 +9,6 @@ const helpers = require('./bss-helpers')
 
 css.helper(helpers)
 
-// todo-james do this as a service
-const updateTitle = Component => 
-	[
-		() => {
-			const heading = document.querySelector('h1,h2')
-			
-			document.title = 
-				"James Forbes" 
-					+ (heading ? ' - ' + heading.innerText : '')
-		}
-	]
-	.map( f => () => setTimeout(f, 1000) )
-	.map( f => ({ attrs }) => 
-		m('div', Object.assign({ oncreate: f }, attrs), m(Component)) 
-	)
-	.map( view => ({ view }))
-	.shift()
-
-
 const routes = {
 	Home: require('./components/home')
 	,Post: require('./components/post')
